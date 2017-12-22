c6d52fbd80e1fe8608b5073eb0791f2f61e80af3
Warren Hood <nullbyte001@gmail.com>
2017-12-22 15:35:16 +0200

    HTML tree editor now functional

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
 global_elts = [];
+doc = new elt("html","/html","document");
+tree_selected = doc;
 function stringrep(char,reps){
   var s = "";
   for(var i = 0;i < reps;i++)s+=char;
   return s;
 };
+function getElt(global_id){
+  return global_elts[global_id];
+}
 function elt(start_tag,end_tag,display_name){
   this.global_id = global_elts.length;
   this.display_name = display_name || start_tag;
@@ -11,14 +16,17 @@ function elt(start_tag,end_tag,display_name){
   this.end_tag = end_tag;
   this.inner = "";
   this.children = [];
+  this.show_children = true;
   this.parent = null;
   this.atts = [];
-  this.makeChild = function(child_start,child_end,child_display){
+  this.makeChild = function(child_start,child_end,child_display,index){
+    if(index !== 0)index = index || this.children.length;
     var child = new elt(child_start,child_end,child_display);
-    return this.addChild(child);
+    return this.addChild(child,index);
   }
-  this.addChild = function(child){
-    this.children.push(child);
+  this.addChild = function(child,index){
+    if(index !== 0)index = index || this.children.length;
+    this.children.splice(index,0,child);
     child.parent = this;
     return child;
   };
@@ -49,20 +57,67 @@ function elt(start_tag,end_tag,display_name){
   global_elts.push(this);
   return this;
 }
+function getChildIndex(elt){
+    for(var i=0;i<elt.parent.children.length;i++)if(elt.parent.children[i]==elt)return i;
+}
+function addAbove(){
+  if((!element_chosen) || tree_selected == doc)return;
+  var toAdd = new elt(element_chosen.start_tag,element_chosen.end_tag,element_chosen.display_name);
+  tree_selected.parent.addChild(toAdd,getChildIndex(tree_selected));
+  update_pane();
+}
+function addBelow(){
+  if((!element_chosen) || tree_selected == doc)return;
+  var toAdd = new elt(element_chosen.start_tag,element_chosen.end_tag,element_chosen.display_name);
+  tree_selected.parent.addChild(toAdd,getChildIndex(tree_selected)+1);
+  update_pane();
+}
+function addInside(){
+  if((!element_chosen))return;
+  var toAdd = new elt(element_chosen.start_tag,element_chosen.end_tag,element_chosen.display_name);
+  tree_selected.addChild(toAdd);
+  update_pane();
+}
+function update_pane(){
+  html_tree_pane.innerHTML = "<table cellspacing='0' cellpadding='0' id='tree_table'>"+ mapOut(doc)+"</table>";
+}
 function mapOut(root,level){
   level = level || 0;
-  var str = stringrep("-",level) + root.display_name + "<br>";
-  for(var i=0; i<root.children.length;i++){
+  var right_click = "var currentElt = getElt("+root.global_id+");\
+    currentElt.show_children = !currentElt.show_children;\
+    update_pane();\
+    return false;";
+  var left_click = "var currentElt = getElt("+root.global_id+");\
+  tree_selected = currentElt;\
+  update_pane();\
+  return false;";
+  var str = "<tr>"+stringrep("<td style='background:rgba(0,0,255,1);'></td>",level) +"<td style='\
+  "+((root == tree_selected)?
+  "color:red;background:white;":
+  "color:black;background:grey;")
+  +
+  "\
+  border:0.5px solid black;border-right:none;'\
+   onclick='"+left_click+"' \
+   oncontextmenu=\""+right_click+"\">"+root.display_name+"</td>" + stringrep("<td style='border:none;'></td>",getDepth(doc)-1-level)+"</tr>";
+  if(root.show_children)for(var i=0; i<root.children.length;i++){
     str += mapOut(root.children[i],level+1);
   };
   return str;
 }
-
-doc = new elt("html","/html","document");
+function getDepth(root,level,most){
+  level = level || 1;
+  most = most || level;
+  for(var i=0;i<root.children.length;i++){
+    possibleMost = getDepth(root.children[i],level+1);
+    if(possibleMost > most)most = possibleMost;
+  }
+  return most;
+}
 doc_head = doc.makeChild("head","/head");
 doc_body = doc.makeChild("body","/body");
 head_style = doc_head.makeChild("style","/style");
-body_para1 = doc_body.makeChild("p","/p");
+body_para1 = doc_body.makeChild("p","/p","paragraph");
 body_div1 = doc_body.makeChild("div","/div");
 div1_span1 = body_div1.makeChild("span","/span");
 div1_div2 = body_div1.makeChild("div","/div");
@@ -167,7 +222,10 @@ function loadElementChooser(){
   for(var i=0; i<elements.length; i++){
     var func = "if(document.getElementsByClassName(\"selected\").length)\
     document.getElementsByClassName(\"selected\")[0].className = \"\";\
-    document.getElementById(\"row"+i+"\").className = \"selected\";";
+    document.getElementById(\"row"+i+"\").className = \"selected\";\
+    element_chosen = {start_tag:\""+elements[i].start_tag+"\",\
+    end_tag:\""+elements[i].end_tag+"\",\
+    display_name:\""+elements[i].display_name+"\"};";
     var mouseover = "var current = document.getElementById(\"row"+i+"\");\
     current.style.background = \"black\";\
     current.style.color = \"orange\";\
@@ -225,4 +283,5 @@ window.onload = function(){
   attribute_head.style.top = window.innerHeight*0.5 + 1 + "px";
   attribute_head.style.left = window.innerWidth*0.7 + 1 + "px";
   loadElementChooser();
+  update_pane();
 }
